bc4eafc60147611c150686b1ff645ba98b568e73
Efe Dortluoğlu <efedortluoglu02@gmail.com>
2023-02-20 10:49:02 +0300

    Added comments

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -30,10 +30,14 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            //We check for whether the name and the display order of the new category are the same. If so we throw an error.
             if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("name", "Username cannot exactly match the display order");
             }
+
+            //We check for if all inputs are valid. If so we add the category to the database, create a tempdata toastr message
+            //for later use and return to the index page.
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
@@ -47,15 +51,17 @@ namespace BulkyBookWeb.Controllers
         //GET
         public IActionResult Delete(int? id)
         {
+            //We check whether id is zero or null since we used a nullable type and if so we throw a not found error.
             if (id == 0 && id == null)
             {
                 return NotFound();
             }
 
+            //Then we use Find() method to find the category from the database based on the id since id is the primary key and
+            // there is no case where there is more than one element having the same id.
+            var categoryFromDb = _db.Categories.Find(id);
             //var categoryFromFirst = _db.Categories.FirstOrDefault(u=>u.Id = id)
             //var categoryFromSingle = _db.Categories.SingleOrDefault(u=>u.Id = id)
-            var categoryFromDb = _db.Categories.Find(id);
-
             if (categoryFromDb == null)
             {
                 return NotFound();
@@ -67,8 +73,9 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Delete(Category obj)
         {
-                _db.Categories.Remove(obj);
-                _db.SaveChanges();
+            //We remove the object from the database and create a tempdata error.
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -7,11 +7,15 @@ namespace BulkyBookWeb.Models
     {
         [Key] // primary key, identity column
         public int Id { get; set; }
+
         [Required] // cannot be null
         public string Name { get; set; }
-        [DisplayName("Display Name")]
+
+        //We want it to have have display name "Display Name" with a space between and also just can range from 1 to 100.
+        [DisplayName("Display Order")]
         [Range(1, 100, ErrorMessage = "Display order must be between 1 and 100")]
         public int DisplayOrder { get; set; }
+
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
